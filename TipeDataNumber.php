102f3b68899f76c4ccc8347b06ea30328858858e
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-08 06:13:08 +0700

    update TipeDataNumber.php tipe data floating point

--- TipeDataNumber.php
+++ TipeDataNumber.php
@@ -13,3 +13,15 @@
 
   echo "Underscore di Number : ";
   var_dump(1_241_241_241);
+
+  echo "Floating Point : ";
+  var_dump(1.234);
+
+  echo "Floating Point dengan E notation Plus (1.7 x 1000) : ";
+  var_dump(1.7e3);
+
+  echo "Floating Point dengan E notation Minus(1.7 x 0.001) : ";
+  var_dump(1.7e-3);
+
+  echo "Underscore di Floating Point : ";
+  var_dump(1_234.567);
