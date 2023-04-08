0ae69d5c113f45906fdabf647b9e9b351ba55156
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-08 23:03:27 +0700

    update TipeDataArray.php array di dalam array

--- TipeDataArray.php
+++ TipeDataArray.php
@@ -21,16 +21,25 @@
   $eko = array(
     "id" => "eko",
     "name" => "Eko Kurniawan",
-    "age" => 30
+    "age" => 30,
+    "address" => array(
+      "city" => "Jakarta",
+      "country" => "Indonesia"
+    )
   );
 
   var_dump($eko);
   var_dump($eko["name"]);
+  var_dump($eko["address"]["country"]);
 
   $budi = [
     "id" => "budi",
     "name" => "Budi Nugraha",
-    "age" => 35
+    "age" => 35,
+    "address" => [
+      "city" => "Jakarta",
+      "country" => "Indonesia"
+    ]
   ];
 
   var_dump($budi);
