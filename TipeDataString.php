f000fc041c229e42257a87feb7543166c960ec6e
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-08 17:33:31 +0700

    update TipeDataString.php multiline string nowdoc

--- TipeDataString.php
+++ TipeDataString.php
@@ -12,3 +12,10 @@
   ini adalah cara ke-3 membuat string
   bisa menggunakan heredoc
   EKO;
+
+  echo <<<'EKO'
+  Selamat belajar PHP,
+  sekarang, kita belajar tipe data string
+  ini adalah cara ke-3 membuat string
+  bisa menggunakan nowdoc
+  EKO;
