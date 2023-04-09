32896151d0abb2ccb8a0033602a12d4938c221a4
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-09 20:27:04 +0700

    update ManipulasiString.php variable parsing

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -16,3 +16,6 @@
   echo $name[0] . PHP_EOL;
   echo $name[1] . PHP_EOL;
   echo $name[2] . PHP_EOL;
+
+  echo "Hello " . $name . ", Selamat Belajar PHP" . PHP_EOL;
+  echo "Hello $name, Selamat Belajar PHP" . PHP_EOL;
