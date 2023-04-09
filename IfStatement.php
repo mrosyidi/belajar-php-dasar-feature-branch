f3b4c2ed1a436b63c128d427a72bacbee910175e
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-10 03:56:23 +0700

    update IfStatement.php else statement

--- IfStatement.php
+++ IfStatement.php
@@ -1,7 +1,9 @@
 <?php
-  $nilai = 80;
+  $nilai = 70;
   $absen = 90;
 
   if($nilai >= 75 & $absen >= 5){
     echo "Selamat Anda Lulus" . PHP_EOL;
+  }else{
+    echo "Maaf, Anda Tidak Lulus" . PHP_EOL;
   }
