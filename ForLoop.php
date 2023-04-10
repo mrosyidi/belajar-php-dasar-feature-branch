a9da3c532f03bc11334ad5ccb64bdd93eda5f961
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-10 14:04:05 +0700

    update ForLoop.php alternatif foor loop

--- ForLoop.php
+++ ForLoop.php
@@ -6,3 +6,11 @@
   for($counter = 10; $counter >= 1; $counter--){
     echo "Ini adalah fr loop ke-$counter" . PHP_EOL;
   }
+
+  for($counter = 1; $counter <= 10; $counter++) :
+    echo "Ini adalah for loop ke-$counter" . PHP_EOL;
+  endfor;
+
+  for($counter = 10; $counter >= 1; $counter--) :
+    echo "Ini adalah fr loop ke-$counter" . PHP_EOL;
+  endfor;
