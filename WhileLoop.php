3fce21e255e1a3ce2dc4ff8543fdb86d65bc9a77
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-10 14:37:58 +0700

    update WhileLoop.php syntax alternatif while loop

--- WhileLoop.php
+++ WhileLoop.php
@@ -5,3 +5,10 @@
     echo "Ini adalah while loop ke-$counter" . PHP_EOL;
     $counter++;
   }
+
+  $counter = 1;
+
+  while($counter <= 10) :
+    echo "Ini adalah while loop ke-$counter" . PHP_EOL;
+    $counter++;
+  endwhile;
