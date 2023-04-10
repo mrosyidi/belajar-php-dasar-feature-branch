08704b6dce6a74e984f6b4bafb2b3959ed5361c2
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-10 07:02:30 +0700

    update TernaryOpeartor.php ternary operator

--- TernaryOperator.php
+++ TernaryOperator.php
@@ -1,11 +1,5 @@
 <?php
   $gender = "PRIA";
   $hi = null;
-
-  if($gender == "PRIA"){
-    $hi = "Hi bro";
-  }else{
-    $hi = "Hi nona";
-  }
-
+  $hi = $gender == "PRIA" ? "Hi bro" : "Hi nona";
   echo $hi . PHP_EOL;
