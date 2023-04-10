b0c7678eacc997de59e71e9c1129e841e0db73a4
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-10 14:00:45 +0700

    update ForLoop.php perulangan dengan post statement

--- ForLoop.php
+++ ForLoop.php
@@ -1,5 +1,8 @@
 <?php
-  for($counter = 1; $counter <= 10;){
+  for($counter = 1; $counter <= 10; $counter++){
     echo "Ini adalah for loop ke-$counter" . PHP_EOL;
-    $counter++;
+  }
+
+  for($counter = 10; $counter >= 1; $counter--){
+    echo "Ini adalah fr loop ke-$counter" . PHP_EOL;
   }
