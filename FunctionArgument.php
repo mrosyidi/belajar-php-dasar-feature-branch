6506325f030a138508b09e2ec7c2a0db1eac7eab
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-11 18:10:06 +0700

    update FunctionArgument.php type declaration

--- FunctionArgument.php
+++ FunctionArgument.php
@@ -7,3 +7,13 @@
   sayHello("Eko");
   sayHello("Budi");
   sayHello("Eko", "Kurniawan");
+
+  function sum(int $first, int $last)
+  {
+    $total = $first + $last;
+    echo "Total $first + $last = $total" . PHP_EOL;
+  }
+
+  sum(100, 100);
+  sum("100", "100");
+  sum(true, false);
