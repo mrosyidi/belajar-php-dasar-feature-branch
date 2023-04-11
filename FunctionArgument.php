3d532f171122aa67ac372f24f51368df2251e4f1
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-11 18:14:34 +0700

    update FunctionArgument.php variable length argument list

--- FunctionArgument.php
+++ FunctionArgument.php
@@ -17,3 +17,16 @@
   sum(100, 100);
   sum("100", "100");
   sum(true, false);
+
+  function sumAll(...$values)
+  {
+    $total = 0;
+    foreach($values as $value){
+      $total += $value;
+    }
+    echo "Total " . implode(" + ", $values) . " = $total" . PHP_EOL;
+  }
+
+  $values = [1, 2, 3, 4, 5];
+  sumAll(1, 2, 3, 4, 5);
+  sumAll(...$values);
