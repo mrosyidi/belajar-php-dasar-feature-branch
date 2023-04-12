1f8010f80af406d9752056cf89fd7e06297f0f74
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-12 17:22:57 +0700

    update FunctionReturnValue.php

--- FunctionReturnValue.php
+++ FunctionReturnValue.php
@@ -9,3 +9,21 @@
 
   $total = sum(100, 100);
   var_dump($total);
+
+  function getFinalValue(int $value)
+  {
+    if($value >= 80){
+      return "A";
+    }else if($value >= 70){
+      return "B";
+    }else if($value >= 60){
+      return "C";
+    }else if($value >= 50){
+      return "D";
+    }else{
+      return "E";
+    }
+  }
+
+  $score = getFinalValue(90);
+  var_dump($score);
