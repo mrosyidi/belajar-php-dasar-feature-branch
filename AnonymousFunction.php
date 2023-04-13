4530da01d2b85649dbd5d20cb65bba38ed8323b1
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-13 14:57:53 +0700

    update AnonymousFunction.php anonymous function sebagai argument

--- AnonymousFunction.php
+++ AnonymousFunction.php
@@ -5,3 +5,19 @@
 
   $sayHello("Eko");
   $sayHello("Budi");
+
+  function sayGoodBye(string $name, $filter)
+  {
+    $finalName = $filter($name);
+    echo "Good Bye $finalName" . PHP_EOL;
+  }
+
+  sayGoodBye("Eko", function(string $name){
+    return strtoupper($name);
+  });
+
+  $filterFunction = function(string $name) : string {
+    return strtoupper($name);
+  };
+
+  sayGoodBye("Eko", $filterFunction);
