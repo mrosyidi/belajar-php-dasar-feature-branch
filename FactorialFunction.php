923409a33de372a4ee98aec3a6972dc1e682d9fb
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-13 16:31:58 +0700

    update FactorialFunction.php factorial recursive

--- FactorialFunction.php
+++ FactorialFunction.php
@@ -11,3 +11,14 @@
 
   var_dump(factorialLoop(5));
   var_dump(1 * 2 * 3 * 4 * 5);
+
+  function factorialRecursive(int $value): int
+  {
+    if($value == 1){
+      return 1;
+    }else{
+      return $value * factorialRecursive($value - 1);
+    }
+  }
+
+  var_dump(factorialRecursive(5));
