3d609c1075be024ae4f13da0fcec8929bfa7366a
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-17 08:40:20 +0700

    add StringFunction.php

---
+++ StringFunction.php
@@ -0,0 +1,7 @@
+<?php
+  var_dump(join(",", [10, 11, 12, 13, 14, 15]));
+  var_dump(explode(" ", "Eko Kurniawan Khannedy"));
+  var_dump(strtolower("EKO KURNIAWAN KHANNEDY"));
+  var_dump(strtoupper("Eko Kurniawan Khannedy"));
+  var_dump("     eko     ");
+  var_dump(substr("Eko Kurniawan Khannedy", 0, 3));
