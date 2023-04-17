954f9121015e01a5b0247c737aa46f34d82cddd9
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-17 14:18:00 +0700

    update Reference.php pass by reference

--- Reference.php
+++ Reference.php
@@ -4,3 +4,12 @@
   $otherName = "Budi";
 
   echo $name . PHP_EOL;
+
+  function increment(int &$value)
+  {
+    $value++;
+  }
+
+  $counter = 1;
+  increment($counter);
+  echo $counter . PHP_EOL;
